0a0fbafdacf2e84ce979463b2d64904d17c9fd1a
teguhap <61332266+teguhap@users.noreply.github.com>
2022-01-04 08:13:44 +0700

    Made SplashScreen,Layout Main & Recycler Layout

---
+++ app/src/main/java/com/project/magramapp/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.project.magramapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //Handler SplashScreen
+        Handler().postDelayed({ Intent(this,MainActivity :: class.java) },2000L)
+
+    }
+}
